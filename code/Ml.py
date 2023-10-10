f24eff729ad802ce78ae835bcfc6b006380649ae
Mohammad Ghavidel <58626257+Slmaking@users.noreply.github.com>
2023-10-10 10:31:11 -0400

    Update Ml.py

--- code/Ml.py
+++ code/Ml.py
@@ -79,3 +79,62 @@ grid_search.fit(X_train, y_train)
 
 # Retrieve the best classifier
 best_tree = grid_search.best_estimator_
+
+
+#Confusion matrix is the best option for multiclass
+---------
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import label_binarize
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load the Iris dataset as an example
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Binarize the true labels (one-hot encoding)
+n_classes = len(np.unique(y))
+y_bin = label_binarize(y, classes=np.unique(y))
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y_bin, test_size=0.5, random_state=42)
+
+# Create a multi-class classifier (OneVsRestClassifier) with a binary classifier (SVC)
+classifier = OneVsRestClassifier(SVC(probability=True, random_state=42))
+
+# Fit the classifier to the training data
+classifier.fit(X_train, y_train)
+
+# Predict probabilities for each class on the test set
+y_score = classifier.predict_proba(X_test)
+
+# Initialize variables to store the false positive rates and true positive rates for each class
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# Plot ROC curves for each class
+plt.figure(figsize=(8, 6))
+colors = ['blue', 'green', 'red']
+for i, color in zip(range(n_classes), colors):
+    plt.plot(fpr[i], tpr[i], color=color, lw=2, label='ROC curve (class {0}) (AUC = {1:0.2f})'
+             ''.format(i, roc_auc[i]))
+
+plt.plot([0, 1], [0, 1], 'k--', lw=2)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curves for Multi-Class Classification')
+plt.legend(loc="lower right")
+plt.show()
+
